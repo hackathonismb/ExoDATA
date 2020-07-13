04e79ac5476d4862394e1ee471c562e6c3cff737
Matthew Sinnott <msinno01@mail.bbk.ac.uk>
2020-07-13 23:11:05 +0100

    webserver_init

---
+++ Matts_ideas/webserver/scoresite/visualise/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+
+CHOICES = [
+    ('1','Covalent Labelling'),
+    ('2','Hydrogen Deuterium Exchange'),
+    ('3','DOPE Score')
+]
+
+HDX_CHOICES = [
+    ('na',''),
+    ('folding','Folding'),
+    ('stability','Stability')
+]
+
+class PDBForm(forms.Form):
+    PDB = forms.CharField(label='PDB',max_length=4,required=False)
+    choice = forms.ChoiceField(label='Experimental Data Type',choices = CHOICES,required=False)
+    hdx_opt = forms.ChoiceField(label='HDX Experiment (HDX Only)',choices=HDX_CHOICES,required=False)
+    data = forms.FileField(label='Input Data',required=False)
---
+++ Matts_ideas/webserver/scoresite/visualise/views.py
@@ -0,0 +1,60 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from .forms import PDBForm
+from .runVis import LoadModel, DOPE, HDXRepr, RepLabels
+import urllib
+
+d_obj = {
+    "1":RepLabels,
+    "2":HDXRepr,
+    "3":DOPE
+}
+
+# Create your views here.
+def index(request):
+
+    if request.method == 'GET':
+        context = {'form':PDBForm()}
+        return render(request,"visualise/home.html",context)
+
+    if request.method == 'POST':
+        form = PDBForm(request.POST, request.FILES)
+
+        print("Files")
+        print(request.FILES)
+        if form.is_valid():
+            input_data = form.cleaned_data
+
+            # if input_data["choice"]=="1":
+                # print(request.FILES['data'].read())
+            obj = d_obj[input_data["choice"]](input_data['PDB'],request.FILES['data'].read())
+            url = obj.open_url(open_link=False,print_out=True,data_label=input_data["hdx_opt"])
+            # print(url)
+
+            # elif input_data["choice"]=="2":
+            #     obj = HDXRepr(input_data['PDB'],request.FILES['data'].read(),)
+
+            print(len(url))
+
+            context = {'state':"\n".join(obj.state)}
+
+            try:
+                req = urllib.request.urlopen(url)
+                assert req.getcode()==20
+                return HttpResponseRedirect(url)
+            except:
+                # return render(request,"visualise/redirect_out.html",context)
+                return HttpResponse("#The output url was too long.\n#Please save this txt file and load it into iCn3D as a statefile.\n" + "\n".join(obj.state),content_type='text/plain')
+
+            # if len(url) <= 2500:
+            #     return HttpResponseRedirect(url)
+            # else:
+            #     return HttpResponse("""
+            #     #The URL generated was too long to open a window.\n
+            #     #Please save this file as a txt and load print_out\n
+            #     #iCn3D as a state file.\n\n
+            #
+            #     %s
+            #     """ % '\n'.join(obj.state))
+
+        return HttpResponse("Testing")
